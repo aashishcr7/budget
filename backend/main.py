a9471228a297f53fe702e3f02284d83e49cec365
Ashish <ashishsinghcr07@gmail.com>
2026-06-03 11:08:26 +0530

    Remove the chat page and updated the env file
    Signed-off-by: Ashish <ashishsinghcr07@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,16 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from routes.auth import router as auth_router
 from routes.trip import router as trip_router
 from routes.profile import router as profile_router
 from fastapi.middleware.cors import CORSMiddleware
-from routes.chat import router as chat_router
 
+load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://trip-planner-alpha-six.vercel.app"],  # frontend URL
+    allow_origins=os.getenv("ALLOWED_ORIGINS").split(","),  # frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -17,4 +19,3 @@ app.add_middleware(
 app.include_router(auth_router)
 app.include_router(trip_router)
 app.include_router(profile_router)
-app.include_router(chat_router)

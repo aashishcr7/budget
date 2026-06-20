4c5e6ac9939a30c18c9446d8c7ad1c5217ffe90d
Ashish <ashishsinghcr07@gmail.com>
2026-06-20 15:18:03 +0530

    updateing middleware
    Signed-off-by: Ashish <ashishsinghcr07@gmail.com>

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,19 +3,19 @@ import { NextRequest, NextResponse } from "next/server";
 const protectedRoutes = ["/dashboard", "/create-trip", "/profile", "/trips"];
 
 export function middleware(request: NextRequest) {
-  // const token = request.cookies.get("access_token");
-  // const pathname = request.nextUrl.pathname;
+  const token = request.cookies.get("access_token");
+  const pathname = request.nextUrl.pathname;
 
-  // console.log("PATH:", request.nextUrl.pathname);
-  // console.log("TOKEN:", token?.value);
+  console.log("PATH:", request.nextUrl.pathname);
+  console.log("TOKEN:", token?.value);
 
-  // const isProtected = protectedRoutes.some((route) =>
-  //   pathname.startsWith(route),
-  // );
+  const isProtected = protectedRoutes.some((route) =>
+    pathname.startsWith(route),
+  );
 
-  // if (isProtected && !token) {
-  //   return NextResponse.redirect(new URL("/login", request.url));
-  // }
+  if (isProtected && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
   return NextResponse.next();
 }
